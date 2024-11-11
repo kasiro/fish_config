00be5b3ba827e8f08bc79b0651beb028549a8ace
kasiro <kon.konikov@mail.ru>
2024-11-11 18:58:26 +0700

    auto upd

--- completions/gul.fish
+++ completions/gul.fish
@@ -1,10 +1,23 @@
-function __fish_gul_no_sub
-    # Здесь можно указать возможные аргументы для вашей команды
-    echo 'install'
-    echo 'add'
-    echo 'check'
-    echo 'init'
+
+function __fish_gul_needs_command
+  set cmd (commandline -opc)
+  if [ (count $cmd) -eq 1 -a $cmd[1] = 'gul' ]
+    return 0
+  end  
+  return 1
 end
 
-complete -c gul -a '(__fish_gul_no_sub)'
-# complete -c gul -n '__fish_use_subcommand'
+function __fish_gul_using_command
+  set cmd (commandline -opc)
+  if [ (count $cmd) -gt 1 ]
+    if [ $argv[1] = $cmd[2] ]
+      return 0
+    end
+  end
+  return 1
+end
+
+complete -c gul -f
+complete -c gul -n '__fish_gul_needs_command' -a 'install add check init'
+complete -c gul -n '__fish_gul_using_command install' -a '.'
+complete -c gul -n '__fish_gul_using_command check' -a '.'
